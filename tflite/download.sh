31b500d9dadbd04550d38241db614df655e77a44
Rosario Laface <rosario.laface@nttdata.com>
2019-12-09 15:47:46 +0100

    Update download.sh

--- tflite/download.sh
+++ tflite/download.sh
@@ -1,4 +1,3 @@
-
 # Get TF Lite model and labels
 wget http://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
 unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -d /root/scripts/models
