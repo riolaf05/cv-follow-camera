3746a7e455d5e9b23274a3ef3e6790cbed618d5a
Rosario Laface <rosario.laface@nttdata.com>
2019-12-09 15:39:11 +0100

    Update tflite_object_detection.py

--- tflite/tflite_object_detection.py
+++ tflite/tflite_object_detection.py
@@ -8,3 +8,76 @@ from annotation import Annotator
 import numpy as np
 import picamera
 
+from PIL import Image
+from tflite_runtime.interpreter import Interpreter
+
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+
+def load_labels(path):
+  """Loads the labels file. Supports files with or without index numbers."""
+  with open(path, 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    labels = {}
+    for row_number, content in enumerate(lines):
+      pair = re.split(r'[:\s]+', content.strip(), maxsplit=1)
+      if len(pair) == 2 and pair[0].strip().isdigit():
+        labels[int(pair[0])] = pair[1].strip()
+      else:
+        labels[row_number] = pair[0].strip()
+  return labels
+
+
+def set_input_tensor(interpreter, image):
+  """Sets the input tensor."""
+  tensor_index = interpreter.get_input_details()[0]['index']
+  input_tensor = interpreter.tensor(tensor_index)()[0]
+  input_tensor[:, :] = image
+
+
+def get_output_tensor(interpreter, index):
+  """Returns the output tensor at the given index."""
+  output_details = interpreter.get_output_details()[index]
+  tensor = np.squeeze(interpreter.get_tensor(output_details['index']))
+  return tensor
+
+
+def detect_objects(interpreter, image, threshold):
+  """Returns a list of detection results, each a dictionary of object info."""
+  set_input_tensor(interpreter, image)
+  interpreter.invoke()
+
+  # Get all output details
+  boxes = get_output_tensor(interpreter, 0)
+  classes = get_output_tensor(interpreter, 1)
+  scores = get_output_tensor(interpreter, 2)
+  count = int(get_output_tensor(interpreter, 3))
+
+  results = []
+  for i in range(count):
+    if scores[i] >= threshold:
+      result = {
+          'bounding_box': boxes[i],
+          'class_id': classes[i],
+          'score': scores[i]
+      }
+      results.append(result)
+  return results
+
+
+def annotate_objects(annotator, results, labels):
+  """Draws the bounding box and label for each object in the results."""
+  for obj in results:
+    # Convert the bounding box figures from relative coordinates
+    # to absolute coordinates based on the original resolution
+    ymin, xmin, ymax, xmax = obj['bounding_box']
+    xmin = int(xmin * CAMERA_WIDTH)
+    xmax = int(xmax * CAMERA_WIDTH)
+    ymin = int(ymin * CAMERA_HEIGHT)
+    ymax = int(ymax * CAMERA_HEIGHT)
+
+    # Overlay the box, label, and score on the camera preview
+    annotator.bounding_box([xmin, ymin, xmax, ymax])
+    annotator.text([xmin, ymin],
+                   '%s\n%.2f' % (labels[obj['class_id']], obj['score']))
+
