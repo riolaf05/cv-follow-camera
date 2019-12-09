c2d920d21bc2c5fc60e68278995e412d0fdb8fa4
Rosario Laface <rosario.laface@nttdata.com>
2019-12-09 16:35:06 +0100

    adding opencv readme

--- tflite/download.sh
+++ tflite/download.sh
@@ -1,6 +1,6 @@
 # Get TF Lite model and labels
 wget http://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
-unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -o -d /home/scripts/models
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -d /home/scripts/models
 rm coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
 
 # Get a labels file with corrected indices, delete the other one
