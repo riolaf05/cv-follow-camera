7ea81fb0f4a8a4b47332f75d5ea2682044e57e32
Rosario Laface <rosario.laface@nttdata.com>
2019-12-09 15:52:59 +0100

    Update download.sh

--- tflite/download.sh
+++ tflite/download.sh
@@ -1,13 +1,13 @@
 # Get TF Lite model and labels
 wget http://storage.googleapis.com/download.tensorflow.org/models/tflite/coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
-unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -o -d /root/scripts/models
+unzip coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip -o -d /home/scripts/models
 rm coco_ssd_mobilenet_v1_1.0_quant_2018_06_29.zip
 
 # Get a labels file with corrected indices, delete the other one
-(cd /root/scripts/models && wget https://dl.google.com/coral/canned_models/coco_labels.txt)
-rm /root/scripts/models/labelmap.txt
+(cd /home/scripts/models && wget https://dl.google.com/coral/canned_models/coco_labels.txt)
+rm /home/scripts/models/labelmap.txt
 
 # Get version compiled for Edge TPU
-(cd /root/scripts/models && wget https://dl.google.com/coral/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite)
+(cd /home/scripts/models && wget https://dl.google.com/coral/canned_models/mobilenet_ssd_v2_coco_quant_postprocess_edgetpu.tflite)
 
-echo -e "Files downloaded to /root/scripts/models"
+echo -e "Files downloaded to /home/scripts/models"
