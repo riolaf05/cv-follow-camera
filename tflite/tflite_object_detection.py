6c5d1b6ad8a9f95efccf66da18c6f472608bb205
Rosario Laface <rosario.laface@nttdata.com>
2019-12-10 18:52:42 +0100

    Update tflite_object_detection.py

--- tflite/tflite_object_detection.py
+++ tflite/tflite_object_detection.py
@@ -89,8 +89,7 @@ def main():
   interpreter.allocate_tensors()
   _, input_height, input_width, _ = interpreter.get_input_details()[0]['shape']
 
-  with picamera.PiCamera(
-      resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
+  with picamera.PiCamera(resolution=(CAMERA_WIDTH, CAMERA_HEIGHT), framerate=30) as camera:
     camera.start_preview()
     try:
       stream = io.BytesIO()
