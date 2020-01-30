c97cecc3c27a9f139bc14d6a29273b1c33a33328
riolaf05 <30504264+riolaf05@users.noreply.github.com>
2020-01-30 22:02:45 +0100

    Update serve_driver.py

--- tflite_edge_tpu/serve_driver.py
+++ tflite_edge_tpu/serve_driver.py
@@ -1,20 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-def SetAngle(angle, servoPIN):
+def setAngle(angle):
 	duty = angle / 18 + 2
-	GPIO.output(03, True)
-	pwm.ChangeDutyCycle(duty)
-	sleep(1)
-	GPIO.output(03, False)
+	return duty
 
 def move_servo(x0):
-
-    servoPIN = 17
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(servoPIN, GPIO.OUT)
-
-    p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
-    p.start(2.5) # Initialization
-    angle = (x0 * 180)/600
-    SetAngle(angle, servoPIN)
+	servoPIN = 17
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(servoPIN, GPIO.OUT)
+	p = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+	p.start(2.5) # Initialization
+	p.ChangeDutyCycle(5)
+	time.sleep(0.5)
+	angle = (x0 * 180)/600
+	duty = setAngle(angle)
+	p.ChangeDutyCycle(duty)
+	time.sleep(0.5)
+	p.stop()
+	GPIO.cleanup()
